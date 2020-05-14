2056e3535eb7a0b9956c1caea36d8ac25adf70fc
Jed Simson <jed.simson@gmail.com>
2020-05-15 10:14:22 +1200

    Add additional default template definitions.

--- RecipeIngredientParser.Core/Templates/TemplateDefinitions.cs
+++ RecipeIngredientParser.Core/Templates/TemplateDefinitions.cs
@@ -7,23 +7,27 @@ namespace RecipeIngredientParser.Core.Templates
     {
         public const string AmountUnitFormIngredient = "{amount} {unit} {form} {ingredient}";
         public const string AmountUnitIngredient = "{amount} {unit} {ingredient}";
+        public const string AmountNoSpaceUnitIngredient = "{amount}{unit} {ingredient}";
         public const string IngredientAmountUnit = "{ingredient}: {amount} {unit}";
         public const string AmountIngredientForm = "{amount} {ingredient}, {form}";
         public const string AmountUnitIngredientForm = "{amount} {unit} {ingredient}, {form}";
         public const string AmountUnitOfFormIngredient = "{amount} {unit} of {form} {ingredient}";
         public const string UnitOfFormIngredient = "{unit} of {form} {ingredient}";
         public const string Ingredient = "{ingredient}";
+        public const string AmountIngredient = "{amount} {ingredient}";
 
         public static readonly string[] DefaultTemplateDefinitions =
         {
             AmountUnitFormIngredient,
             AmountUnitIngredient,
+            AmountNoSpaceUnitIngredient,
             IngredientAmountUnit,
             AmountIngredientForm,
             AmountUnitIngredientForm,
             AmountUnitOfFormIngredient,
             UnitOfFormIngredient,
-            Ingredient
+            Ingredient,
+            AmountIngredient
         };
     }
 }
