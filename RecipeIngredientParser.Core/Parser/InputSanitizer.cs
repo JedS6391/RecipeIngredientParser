5825179c88b2f9f34cbb4190dc28638ca35ef515
Jed Simson <jed.simson@gmail.com>
2020-05-15 14:58:18 +1200

    Sanitize unicode fractions with non-unicode replacements.

--- RecipeIngredientParser.Core/Parser/InputSanitizer.cs
+++ RecipeIngredientParser.Core/Parser/InputSanitizer.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace RecipeIngredientParser.Core.Parser
@@ -22,6 +24,32 @@ namespace RecipeIngredientParser.Core.Parser
         /// Matches a string with any characters between brackets (e.g. '(text)').
         /// </summary>
         private static readonly Regex BracketedTextRegex = new Regex(@"(\(.*?\))", RegexOptions.Compiled);
+
+        /// <summary>
+        /// A set of fractions that have unicode representations which we want to replace with a non-unicode representation.
+        /// </summary>
+        private static readonly Dictionary<string, string> UnicodeFractionReplacementTable = new Dictionary<string, string>()
+        {
+            { "¼", "1/4" },
+            { "½", "1/2" },
+            { "¾", "3/4" },
+            { "⅐", "1/7" },
+            { "⅑", "1/9" },
+            { "⅒", "1/10" },
+            { "⅓", "1/3" },
+            { "⅔", "2/3" },
+            { "⅕", "1/5" },
+            { "⅖", "2/5" },
+            { "⅗", "3/5" },
+            { "⅘", "4/5" },
+            { "⅙", "1/6" },
+            { "⅚", "5/6" },
+            { "⅛", "1/8" },
+            { "⅜", "3/8" },
+            { "⅝", "5/8" },
+            { "⅞", "7/8" },
+            { "↉", "0/3" }
+        };
         
         /// <summary>
         /// Sanitizes the input by removing extraneous characters and substituting common patterns with known tokens.
@@ -45,8 +73,20 @@ namespace RecipeIngredientParser.Core.Parser
             // Remove extraneous spaces a second time in case substitutions introduced any spaces.
             input = SpacesRegex.Replace(input, " ");
 
+            // Replace any unicode fractions with non-unicode versions
+            // '½' -> '1/2'
+            input = ReplaceUnicodeFractions(input);
+
             // Always convert to lower case.
             return input.ToLower();
+        }
+
+        private static string ReplaceUnicodeFractions(string input)
+        {
+            return UnicodeFractionReplacementTable.Aggregate(
+                input, 
+                (current, replacement) => 
+                    current.Replace(replacement.Key, replacement.Value));
         }
     }
 }
