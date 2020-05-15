0fe1f98677bf93014bc8b02a478ca6fe895f3653
Jed Simson <jed.simson@gmail.com>
2020-05-15 18:36:18 +1200

    Add further validation to real data integration tests - still some cases failing.

--- RecipeIngredientParser.Test/Integration/RealDataIngredientParserTests.cs
+++ RecipeIngredientParser.Test/Integration/RealDataIngredientParserTests.cs
@@ -17,7 +17,8 @@ namespace RecipeIngredientParser.Test.Integration
         [TestCaseSource(nameof(_vegetarianBlackBeanEnchiladas))]
         public void IngredientParser_TryReadRealWorldDataIngredients_ShouldSuccessfullyParseIngredient(
             string rawIngredient,
-            bool expectedResult)
+            bool expectedResult,
+            ParseResult.IngredientDetails expectedDetails)
         {
             var parserStrategies = new IParserStrategy[]
             {
@@ -39,6 +40,11 @@ namespace RecipeIngredientParser.Test.Integration
             if (expectedResult)
             {
                 Assert.AreEqual(TemplateMatchResult.FullMatch, parseResult.Metadata.MatchResult);
+                Assert.IsNotNull(parseResult);
+                Assert.AreEqual(expectedDetails.Amount, parseResult.Details.Amount);
+                Assert.AreEqual(expectedDetails.Unit, parseResult.Details.Unit);
+                Assert.AreEqual(expectedDetails.Form, parseResult.Details.Form);
+                Assert.AreEqual(expectedDetails.Ingredient, parseResult.Details.Ingredient);
             }
             else
             {
@@ -52,8 +58,10 @@ namespace RecipeIngredientParser.Test.Integration
         {
             switch (token)
             {
-                case LiteralToken _:
-                    return 0.0m;
+                case LiteralToken literalToken:
+                    // Longer literals score more - the assumption being that
+                    // a longer literal means a more specific value.
+                    return 0.1m * literalToken.Value.Length;
                     
                 case LiteralAmountToken _:
                 case FractionalAmountToken _:
@@ -86,104 +94,209 @@ namespace RecipeIngredientParser.Test.Integration
             {
                 "1 Full Recipe Vegan Taco Meat",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "full recipe vegan taco meat"
+                }
             },
             new dynamic[]
             {
                 "1 Tbsp Olive Oil",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "tbsp",
+                    Form = null,
+                    Ingredient = "olive oil"
+                }
             },
             new dynamic[]
             {
                 "1/2 Onion (Chopped)",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1/2",
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "onion"
+                }
             },            
             new dynamic[]
             {
                 "1/4 tsp Cayenne Pepper",
                 // Successful?
-                true
-            },
-            new dynamic[]
-            {
-                "1/4 tsp Cayenne Pepper",
-                // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1/4",
+                    Unit = "tsp",
+                    Form = null,
+                    Ingredient = "cayenne pepper"
+                }
             },
             new dynamic[]
             {
                 "1/4 tsp Chili Flakes",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1/4",
+                    Unit = "tsp",
+                    Form = null,
+                    Ingredient = "chili flakes"
+                }
             },
             new dynamic[]
             {
                 "1/2 tsp Cumin",
                 // Successful?
-                true
-            },      
-            // TODO: Figure out a way to get this test case to parse successfully.
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1/2",
+                    Unit = "tsp",
+                    Form = null,
+                    Ingredient = "cumin"
+                }
+            },
             new dynamic[]
             {
                 "1 15oz (425g) Can Black Beans (Drained)",
                 // Successful?
-                false
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "can",
+                    Form = null,
+                    Ingredient = "black beans"
+                }
             },
             new dynamic[]
             {
                 "2 Tbsp Water",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "2",
+                    Unit = "tbsp",
+                    Form = null,
+                    Ingredient = "water"
+                }
             },
             new dynamic[]
             {
                 "Sea Salt and Black Pepper (To Taste)",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = null,
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "sea salt and black pepper"
+                }
             },  
             new dynamic[]
             {
                 "1 Full Recipe Pico De Gallo",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "full recipe pico de gallo"
+                }
             },  
             new dynamic[]
             {
                 "1 Full Recipe Vegan Nacho Cheese",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "full recipe vegan nacho cheese"
+                }
             },  
             new dynamic[]
             {
                 "2 Avocados",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "2",
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "avocados"
+                }
             },
             new dynamic[]
             {
                 "1 Tbsp Fresh Lime Juice",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "tbsp",
+                    Form = null,
+                    Ingredient = "fresh lime juice"
+                }
             },
             new dynamic[]
             {
                 "Sea Salt and Black Pepper (To Taste)",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = null,
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "sea salt and black pepper"
+                }
             },
             new dynamic[]
             {
                 "16oz (450g) Tortilla Chips",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "16",
+                    Unit = "oz",
+                    Form = null,
+                    Ingredient = "tortilla chips"
+                }
             },
             new dynamic[]
             {
                 "Fresh Cilantro",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = null,
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "fresh cilantro"
+                }
             }
         };
 
@@ -193,85 +306,183 @@ namespace RecipeIngredientParser.Test.Integration
             {
                 "2 cups homemade enchilada sauce",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "2",
+                    Unit = "cups",
+                    Form = null,
+                    Ingredient = "homemade enchilada sauce"
+                }
             },
             new dynamic[]
             {
                 "2 tablespoons olive oil",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "2",
+                    Unit = "tablespoons",
+                    Form = null,
+                    Ingredient = "olive oil"
+                }
             },
             new dynamic[]
             {
                 "1 cup chopped red onion (about 1 small red onion)",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "cup",
+                    Form = "chopped",
+                    Ingredient = "red onion"
+                }
             },
             new dynamic[]
             {
                 "1 red bell pepper, chopped",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = null,
+                    Form = "chopped",
+                    Ingredient = "red bell pepper"
+                }
             },
             new dynamic[]
             {
                 "1 bunch of broccoli or 1 small head of cauliflower (about 1 pound), florets removed and sliced into small, bite-sized pieces",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "bunch",
+                    Form = null,
+                    Ingredient = "broccoli"
+                }
             },
             new dynamic[]
             {
                 "1 teaspoon Frontier Co-op Ground Cumin",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "teaspoon",
+                    Form = null,
+                    Ingredient = "frontier co-op ground cumin"
+                }
             },
             new dynamic[]
             {
                 "¼ teaspoon Frontier Co-op Ground Cinnamon",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1/4",
+                    Unit = "teaspoon",
+                    Form = null,
+                    Ingredient = "frontier co-op ground cinnamon"
+                }
             },
             new dynamic[]
             {
                 "5 to 6 ounces baby spinach (about 5 cups, packed)",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "5-6",
+                    Unit = "ounces",
+                    Form = null,
+                    Ingredient = "baby spinach"
+                }
             },
             new dynamic[]
             {
                 "1 can (15 ounces) black beans, drained and rinsed, or 1 ½ cups cooked black beans",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "can",
+                    Form = "drained and rinsed",
+                    Ingredient = "black beans"
+                }
             },
             new dynamic[]
             {
                 "1 cup shredded Monterey Jack cheese, divided",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1",
+                    Unit = "cup",
+                    Form = "shredded",
+                    Ingredient = "monterey jack cheese"
+                }
             },
             new dynamic[]
             {
                 "½ teaspoon salt, to taste",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "1/2",
+                    Unit = "teaspoon",
+                    Form = null,
+                    Ingredient = "salt"
+                }
             },
             new dynamic[]
             {
                 "Freshly ground black pepper, to taste",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = null,
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "freshly ground black pepper"
+                }
             },
             new dynamic[]
             {
                 "8 whole wheat tortillas (about 8” in diameter)",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = "8",
+                    Unit = null,
+                    Form = null,
+                    Ingredient = "whole wheat tortillas"
+                }
             },
             new dynamic[]
             {
                 "Handful of chopped cilantro, for garnishing",
                 // Successful?
-                true
+                true,
+                new ParseResult.IngredientDetails()
+                {
+                    Amount = null,
+                    Unit = "handful",
+                    Form = "chopped",
+                    Ingredient = "cilantro"
+                }
             }
         };
 
